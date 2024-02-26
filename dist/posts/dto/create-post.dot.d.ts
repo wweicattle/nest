3b83e5f714c81a0566aa43a0c705c4337c0e1525
wuwei <wuwei@taqu.cn>
2024-02-26 09:26:30 +0800

    fix: delete node_modules

--- dist/posts/dto/create-post.dot.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreatePostDto {
-    readonly title: string;
-    readonly author: string;
-    readonly content: string;
-    readonly cover_url: string;
-    readonly type: number;
-}
